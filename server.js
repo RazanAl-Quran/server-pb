b476533bff20ae881a34b2c2c89c206223e3d142
RazanAl-Quran <quraan_razan@hotmail.com>
2022-11-02 21:35:06 +0200

    server done

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+
+app.get('/',(rew,res)=>{
+    res.status(200).send('all is great')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
